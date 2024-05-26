87a73be32e8af20b0739f488ea4e921259fe4be9
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-05-26 16:01:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/stepup/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.stepup
+
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity(), SensorEventListener {
+    private var sensorManager: SensorManager? = null
+    private var running = false
+    private var totalSteps = 0f
+    private var previousTotalSteps = 0f
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        SensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        TODO("Not yet implemented")
+    }
+}
