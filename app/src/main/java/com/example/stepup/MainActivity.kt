d3de91fc5c3ff14a8457db299951319210010283
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-05-26 18:07:09 +0530

    app ready

--- app/src/main/java/com/example/stepup/MainActivity.kt
+++ app/src/main/java/com/example/stepup/MainActivity.kt
@@ -1,82 +1,130 @@
 package com.example.stepup
 
 import android.content.Context
-import android.content.SharedPreferences
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity(), SensorEventListener {
+class MainActivity : AppCompatActivity(),SensorEventListener  {
+    // Added SensorEventListener the MainActivity class
+    // Implement all the members in the class MainActivity
+    // after adding SensorEventListener
+
+    // we have assigned sensorManger to nullable
     private var sensorManager: SensorManager? = null
-    private var running = false
-    private var totalSteps = 0f
-    private var previousTotalSteps = 0f
-    var tv_stepsTaken = findViewById<TextView>(R.id.tv_stepsTaken)
-    var progress_circular = findViewById<CircularProgressView>(R.id.progress_circular)
-    var tv_goal = findViewById<TextView>(R.id.tv_goal)
 
+    // Creating a variable which will give the running status
+    // and initially given the boolean value as false
+    private var running = false
+
+    // Creating a variable which will counts total steps
+    // and it has been given the value of 0 float
+    private var totalSteps = 0f
+
+    // Creating a variable  which counts previous total
+    // steps and it has also been given the value of 0 float
+    private var previousTotalSteps = 0f
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
 
+        loadData()
+        resetSteps()
+
+        // Adding a context of SENSOR_SERVICE as Sensor Manager
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
     }
 
     override fun onResume() {
         super.onResume()
         running = true
-        val stepSensor: Sensor? = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
 
-        if(stepSensor == null) {
-            Toast.makeText(this, "No Sensor detected on this device", Toast.LENGTH_SHORT).show()
-        }
-        else{
+        // Returns the number of steps taken by the user since the last reboot while activated
+        // This sensor requires permission android.permission.ACTIVITY_RECOGNITION.
+        // So don't forget to add the following permission in AndroidManifest.xml present in manifest folder of the app.
+        val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+
+        if (stepSensor == null) {
+            // This will give a toast message to the user if there is no sensor in the device
+            Toast.makeText(this, "No sensor detected on this device", Toast.LENGTH_SHORT).show()
+        } else {
+            // Rate suitable for the user interface
             sensorManager?.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI)
         }
     }
+
     override fun onSensorChanged(event: SensorEvent?) {
-        if(running) {
+
+        // Calling the TextView that we made in activity_main.xml
+        // by the id given to that TextView
+        var tv_stepsTaken = findViewById<TextView>(R.id.tv_stepsTaken)
+
+        if (running) {
             totalSteps = event!!.values[0]
+
+            // Current steps are calculated by taking the difference of total steps
+            // and previous steps
             val currentSteps = totalSteps.toInt() - previousTotalSteps.toInt()
+
+            // It will show the current steps to the user
             tv_stepsTaken.text = ("$currentSteps")
-
-
-            progress_circular.apply {
-                setProgressWithAnimation(currentSteps.toFloat())
-            }
         }
     }
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        TODO("Not yet implemented")
-    }
 
-    private fun resetSteps(){
-        tv_stepsTaken.setOnClickListener{
-            Toast.makeText(this, "Long Tap to reset Steps", Toast.LENGTH_SHORT).show()
+    fun resetSteps() {
+        var tv_stepsTaken = findViewById<TextView>(R.id.tv_stepsTaken)
+        tv_stepsTaken.setOnClickListener {
+            // This will give a toast message if the user want to reset the steps
+            Toast.makeText(this, "Long tap to reset steps", Toast.LENGTH_SHORT).show()
         }
 
-        tv_stepsTaken.setOnLongClickListener{
+        tv_stepsTaken.setOnLongClickListener {
+
             previousTotalSteps = totalSteps
+
+            // When the user will click long tap on the screen,
+            // the steps will be reset to 0
             tv_stepsTaken.text = 0.toString()
+
+            // This will save the data
             saveData()
-            Toast.makeText(this, "Long Tap to reset Steps", Toast.LENGTH_SHORT).show()
+
             true
         }
     }
-    private fun setProgressWithAnimation(toFloat: Float) {
-        val sharedPreferences: SharedPreferences =
-            getSharedPreferences("my Preferences", Context.MODE_PRIVATE)
-        val editor:SharedPreferences.Editor = sharedPreferences.edit()
-        editor.putFloat("Key1", previousTotalSteps)
+
+    private fun saveData() {
+
+        // Shared Preferences will allow us to save
+        // and retrieve data in the form of key,value pair.
+        // In this function we will save data
+        val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+
+        val editor = sharedPreferences.edit()
+        editor.putFloat("key1", previousTotalSteps)
         editor.apply()
     }
 
+    private fun loadData() {
+
+        // In this function we will retrieve data
+        val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+        val savedNumber = sharedPreferences.getFloat("key1", 0f)
+
+        // Log.d is used for debugging purposes
+        Log.d("MainActivity", "$savedNumber")
+
+        previousTotalSteps = savedNumber
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        // We do not have to write anything in this function for this app
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,5 +19,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Step Up"
-include ':app'
+rootProject.name = "StepUp"
+include(":app")
+ 
