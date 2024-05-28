3cf951aa2b7e2175bd6669034cc03be6cf7c23fc
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-05-28 23:38:34 +0530

    login screen done

--- app/src/main/java/com/example/stepup/LoginActivity.kt
+++ app/src/main/java/com/example/stepup/LoginActivity.kt
@@ -1,20 +1,70 @@
 package com.example.stepup
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.stepup.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class LoginActivity : AppCompatActivity() {
+    private val binding: ActivityLoginBinding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
+    private lateinit var auth: FirebaseAuth
+
+    override fun onStart() {
+        super.onStart()
+
+        //check if already loged in
+        val currentUser: FirebaseUser? = auth.currentUser
+        if (currentUser != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        //Initialise firebase auth
+        auth = FirebaseAuth.getInstance()
+
+
+
+        binding.LoginButton.setOnClickListener {
+            val signInEmail = binding.signinEmail.text.toString()
+            val signInPassword = binding.signInPassword.text.toString()
+
+            if (signInEmail.isEmpty() || signInPassword.isEmpty()) {
+                Toast.makeText(this, "Please enter all details", Toast.LENGTH_SHORT).show()
+            } else {
+                auth.signInWithEmailAndPassword(signInEmail, signInPassword)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this, "Sign-IN successful", Toast.LENGTH_SHORT).show()
+                            startActivity(Intent(this, MainActivity::class.java))
+                            finish()
+                        } else {
+                            Toast.makeText(
+                                this,
+                                "Sign-In failed: ${task.exception?.message}",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+
+                    }
+            }
+        }
+
+        binding.SignupButton.setOnClickListener{
+            startActivity(Intent(this, signupActivity::class.java))
+            finish()
         }
     }
 }
